44e4bc2f2ffd8f63d4f7180c62609bcb63c096f2
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2023-07-11 18:13:08 +0700

    update
    update

--- routes/history.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controllers = require('../controllers')
-
-router.get('/history', controllers.history.viewHistory)
-router.get('/allHistory', controllers.history.allTransc)
-router.get('/history/:id_transaction', controllers.history.cancelTransc)
-
-module.exports =router
