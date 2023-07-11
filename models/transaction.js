f08e080949ec68fdef00c4fe8a11fa4a863397dd
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2023-07-11 21:42:03 +0700

    update

--- models/transaction.js
+++ models/transaction.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
+import mysql2 from 'mysql2'
 const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`);
 const user = require('./user')
 
